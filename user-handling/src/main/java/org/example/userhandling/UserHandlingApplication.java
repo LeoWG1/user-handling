b1b42e1b35649d33bd29fec13b895055f7a55399
LeoWG1 <leo.gestblom@gmail.com>
2025-10-28 14:44:28 +0100

    Add files via upload

---
+++ user-handling/src/main/java/org/example/userhandling/UserHandlingApplication.java
@@ -0,0 +1,13 @@
+package org.example.userhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserHandlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserHandlingApplication.class, args);
+    }
+
+}
